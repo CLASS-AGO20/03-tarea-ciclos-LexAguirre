c5670031245af20e823a15418468cffe35e5f20d
Ramon Aguirre <raguirre3@ucol.mx>
2020-12-11 13:45:22 -0600

    implemented Suma

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
 export default class App {
     sumatoriaSerieUno(numero) {
+       let suma = 0;
 
+        for(let i = 1; i <= numero; i++) {
+        
+            suma = suma + 1/i;
+            
+        }
+
+        return suma
     }
-
+ 
     sumatoriaSerieDos(numero) {
+        let i = 1
+        let suma = 0
 
+        while(i <= numero) {
+
+            if(i % 2 == 0){
+                suma = suma + 1/i;
+            } else {
+                suma = suma - 1/i;
+            }
+        }
+
+        return suma
     }
 
     esPrimo(numero) {
-
+        do{
+            if(numero % 1 == 1) {
+                return true 
+            } else if (numero % numero == 1) {
+                return true;
+            } else if (numero == 2) {
+                return false
+            }
+        } while()
     }
 
     obtenerMultiplos(inicio, fin) {
@@ -16,8 +44,19 @@ export default class App {
     }
 
     obtenerImpares(numero1, numero2) {
-        
+
     }
 }
 
 let app = new App();
+
+console.log("Implemented sumatoriaSerieUno");
+console.log(app.sumatoriaSerieUno(7));
+console.log("Implemented sumatoriaSerieDos");
+console.log(app.sumatoriaSerieDos());
+console.log("Implemented esPrimo");
+console.log(app.esPrimo());
+console.log("Implemented obtenerMultiplos");
+console.log(app.obtenerMultiplos());
+console.log("Implemented obtenerImpares");
+console.log(app.obtenerImpares());
