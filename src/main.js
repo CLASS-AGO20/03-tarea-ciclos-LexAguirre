84001be8e9fd64dc048360f1497c5de2ea1dabb3
Ramon Aguirre <raguirre3@ucol.mx>
2020-12-11 15:09:10 -0600

    Implemented esPrimo

--- src/main.js
+++ src/main.js
@@ -32,22 +32,104 @@ export default class App {
     esPrimo(numero) {
 
         do{
-            if(numero % 1 == 1) {
+
+            
+            /*if(numero % 2 === 0) {
                 return true 
-            } else if (numero % numero == 1) {
+            } else if (numero % 3 == 0) {
                 return true;
-            } else if (numero == 2) {
+            } else if (numero % 5 == 0) {
+                return true;
+            } else if (numero % 7 == 0) {
+                return true;
+            } else if (numero % 11 == 0) {
+                return true;
+            } else if (numero % 13 == 0) {
+                return true;
+            } else if (numero % 17 == 0) {
+                return true;
+            } else if (numero % 19 == 0) {
+                return true;
+            } else if (numero % 23 == 0) {
+                return true;
+            } else if (numero % 29 == 0) {
+                return true;
+            } else if (numero % 31 == 0) {
+                return true;
+            } else if (numero % 37 == 0) {
+                return true;
+            } else if (numero % 41 == 0) {
+                return true;
+            } else if (numero % 43 == 0) {
+                return true;
+            } else if (numero % 47 == 0) {
+                return true;
+            } else if (numero % 53 == 0) {
+                return true;
+            } else if (numero % 59 == 0) {
+                return true;
+            } else if (numero % 61 == 0) {
+                return true;
+            } else if (numero % 67 == 0) {
+                return true;
+            } else if (numero % 71 == 0) {
+                return true;
+            } else if (numero % 73 == 0) {
+                return true;
+            } else {
                 return false
-            }
-        } while(not)
+            }*/
+        } while(numero);
     }
 
     obtenerMultiplos(inicio, fin) {
 
+        if(inicio > fin) {
+            let t = inicio;
+            inicio = fin;
+            fin = t;
+        }
+        
+        let i = inicio;
+        let imp = 0;
+
+        do {
+            if (i % 3 == 0) {
+                imp = imp + i;
+                imp = imp.toString();
+            }
+
+            i++;
+
+        } while (i <= fin);
+
+        return imp;
     }
 
     obtenerImpares(numero1, numero2) {
 
+        let imp = 0; 
+
+        if(numero2 > numero1) {
+            let t = numero2;
+            numero2 = numero1;
+            numero1 = t;
+        }
+        
+        for(let i = numero1; i >= numero2; i--) {
+            if(i % 2 != 0){
+
+                if(imp == 0){
+                    imp = imp + i;
+                    imp = imp.toString();
+                } else {
+                    imp = imp + "," + i;
+                    imp = imp.toString();
+                }
+                
+            }
+        }
+        return imp; 
     }
 }
 
@@ -58,8 +140,11 @@ console.log(app.sumatoriaSerieUno(7));
 console.log("Implemented sumatoriaSerieDos");
 console.log(app.sumatoriaSerieDos(2));
 console.log("Implemented esPrimo");
-console.log(app.esPrimo());
+console.log(app.esPrimo(3));
+console.log(app.esPrimo(20));
 console.log("Implemented obtenerMultiplos");
-console.log(app.obtenerMultiplos());
+console.log(app.obtenerMultiplos(10, 25));
+console.log(app.obtenerMultiplos(25, 10));
 console.log("Implemented obtenerImpares");
-console.log(app.obtenerImpares());
+console.log(app.obtenerImpares(4, 13));
+console.log(app.obtenerImpares(13, 4));
